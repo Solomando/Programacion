6e0d63cbfe1b27c4dd53f7a552c7305f54530e4d
Ann <angel.solomando@gmail.com>
2024-12-02 11:42:52 +0100

    creacion

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       static class vehiculo {       
+        
+       }
+       public vehiculo {
+
+       }
+       public Vehiculo(String Ptipo, String Pmodelo, int PnroRuedas, double Pvelocidad) {
+       }
+       ¨
+    }
+}
