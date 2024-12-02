a62fdae96a97390273176fc47ec28232576d9b39
Ann <angel.solomando@gmail.com>
2024-12-02 12:06:20 +0100

    modificacion

--- src/App.java
+++ src/App.java
@@ -5,13 +5,13 @@ public class App {
         Vehiculo volvoCamion = new Vehiculo("Camión", "Volvo 4500", 8, 50.3);
         Vehiculo bicicletaBH = new Vehiculo("Bicicleta", "BH Family", 2, 0.0);
 
-        // Mostrar vehículos iniciales
+        // Mostrar vehículos iniciales por pantalla
         System.out.println("Vehículos iniciales:");
         System.out.println(seatPanda);
         System.out.println(volvoCamion);
         System.out.println(bicicletaBH);
 
-        // Acelerar todos los vehículos
+        // Acelerar todos los vehículos por pantalla
         System.out.println("\nAcelerando todos los vehículos en 10 km/h:");
         seatPanda.acelerar(10);
         volvoCamion.acelerar(10);
